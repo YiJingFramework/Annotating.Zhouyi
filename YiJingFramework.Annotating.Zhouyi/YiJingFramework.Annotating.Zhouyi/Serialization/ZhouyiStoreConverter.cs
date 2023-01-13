8185fcea176c64ec9adcb61e7aa8b3e66f2d5a71
yueyinqiu <lujiaqi2003@126.com>
2023-01-13 14:47:53 +0800

    trimmable

--- YiJingFramework.Annotating.Zhouyi/YiJingFramework.Annotating.Zhouyi/Serialization/ZhouyiStoreConverter.cs
+++ YiJingFramework.Annotating.Zhouyi/YiJingFramework.Annotating.Zhouyi/Serialization/ZhouyiStoreConverter.cs
@@ -1,22 +1,40 @@
-﻿using System.Reflection.PortableExecutable;
-using System.Text.Json;
+﻿using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace YiJingFramework.Annotating.Zhouyi.Serialization
 {
-    internal sealed class ZhouyiStoreConverter : JsonConverter<ZhouyiStore>
+    /// <summary>
+    /// 用于 <seealso cref="ZhouyiStore"/> 的 <seealso cref="JsonConverter"/> 。
+    /// <seealso cref="JsonConverter"/> for <seealso cref="ZhouyiStore"/>.
+    /// </summary>
+    public sealed class ZhouyiStoreConverter : JsonConverter<ZhouyiStore>
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="reader"></param>
+        /// <param name="typeToConvert"></param>
+        /// <param name="options"></param>
+        /// <returns></returns>
         public override ZhouyiStore? Read(
             ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            var store = JsonSerializer.Deserialize<AnnotationStore>(ref reader, options);
+            var context = new AnnotationStoreSerializerContext(options);
+            var store = JsonSerializer.Deserialize(ref reader, context.AnnotationStore);
             return new ZhouyiStore(store);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="writer"></param>
+        /// <param name="value"></param>
+        /// <param name="options"></param>
         public override void Write(
             Utf8JsonWriter writer, ZhouyiStore value, JsonSerializerOptions options)
         {
-            JsonSerializer.Serialize(writer, value.Store, options);
+            var context = new AnnotationStoreSerializerContext(options);
+            JsonSerializer.Serialize(writer, value.Store, context.AnnotationStore);
         }
     }
 }
